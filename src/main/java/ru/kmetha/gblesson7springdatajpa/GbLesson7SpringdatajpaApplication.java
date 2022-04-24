b32cd37cdf473a623c0ce034f5986c1592eb6305
Tenthy <rakursdead@gmail.com>
2022-04-24 12:10:31 +0300

    Initial commit

---
+++ src/main/java/ru/kmetha/gblesson7springdatajpa/GbLesson7SpringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package ru.kmetha.gblesson7springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbLesson7SpringdatajpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbLesson7SpringdatajpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kmetha/gblesson7springdatajpa/GbLesson7SpringdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kmetha.gblesson7springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbLesson7SpringdatajpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
